509966e2d7651c2b1896920087981d6b040a9a39
shantanuprasadbtech <109209229+shantanuprasadbtech@users.noreply.github.com>
2023-05-07 10:41:09 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,35 +1,35 @@
-public class ThreadCreation extends Thread {
-    public void run() {
-        System.out.println("Thread is running.");
-    }
-
-    public static void main(String[] args) {
-        ThreadCreation thread = new ThreadCreation();
-        thread.start(); // Start the thread
-
-        // Wait for the thread to finish
-        try {
-            thread.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+public class Main extends Thread{
+        public void run() {
+            System.out.println("Thread is running.");
         }
 
-        // Check if the thread is alive
-        System.out.println("Thread is alive: " + thread.isAlive());
+        public static void main(String[] args) {
+            Main thread = new Main();
+            thread.start(); // Start the thread
 
-        // Set the thread name
-        thread.setName("MyThread");
+            // Wait for the thread to finish
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
-        // Get the thread name
-        System.out.println("Thread name: " + thread.getName());
+            // Check if the thread is alive
+            System.out.println("Thread is alive: " + thread.isAlive());
 
-        // Get the thread priority
-        System.out.println("Thread priority: " + thread.getPriority());
+            // Set the thread name
+            thread.setName("Test Thread");
 
-        // Set the thread priority
-        thread.setPriority(Thread.MAX_PRIORITY);
+            // Get the thread name
+            System.out.println("Thread name: " + thread.getName());
 
-        // Get the thread state
-        System.out.println("Thread state: " + thread.getState());
+            // Get the thread priority
+            System.out.println("Thread priority: " + thread.getPriority());
+
+            // Set the thread priority
+            thread.setPriority(Thread.MAX_PRIORITY);
+
+            // Get the thread state
+            System.out.println("Thread state: " + thread.getState());
+        }
     }
-}
